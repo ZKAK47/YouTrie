8c9dda462cfdcbb3b775cea0fcb65ea120e81c4f
ZKAK47 <zkd80541@gmail.com>
2026-04-10 22:07:55 +0200

    Add lazy loading to imgs

--- src/public/scripts/playlist-store.js
+++ src/public/scripts/playlist-store.js
@@ -483,6 +483,7 @@ function createVideoNode(v) {
         if (v.thumbnail) {
             const img = document.createElement("img");
             img.src = v.thumbnail;
+            img.loading = "lazy"
             img.alt = v.title;
             thumbnailWrapper.appendChild(img);
         }
